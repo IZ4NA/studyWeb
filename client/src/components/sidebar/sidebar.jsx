2fad7c8d4219bcf11bec73b24043129b36a4bd08
IZ4NA <jovijukes.cvg@gmail.com>
2025-09-27 10:40:26 -0300

    algumas coisa up

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -8,9 +8,52 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="conteiner-sidebar">
-       <img src={Logo} alt="fail" />
 
-       <a href="/home">Home</a>
+        <div className="top-logo">
+       <img src={Logo} alt="fail" />
+        {/* <p>IZANASTUDY</p> */}
+        </div>
+
+      <div>
+        <h2>API rest</h2>
+       <ul className="rotasApis">
+        <li><a href="/gets">Get</a></li>
+        <li><a href="/posts">Post</a></li>
+        <li><a href="/puts">Put</a></li>
+        <li><a href="/deletes">Deletes</a></li>
+       </ul>
+      </div>
+
+
+      <div>
+        <a href="/dialogs">Dialogs</a>
+      </div>
+
+
+      <div>
+        <h2>Hoks</h2>
+        <ul>
+          <li><a href="/usestates">UseState</a></li>
+          <li><a href="/useefects">UseEfect</a></li>
+          <li><a href="/usecontexts">UseContext</a></li>
+        </ul>
+      </div>
+
+
+      <div>
+        <a href="/cruds">Cruds</a>
+      </div>
+
+
+      <div className="conteiner-perfil">
+        <div className="perfil">
+          <div className="foto"></div>
+          <p>nome</p>
+          <p>aluno full-stack</p>
+
+          <p>sair</p>
+        </div>
+      </div>
 
       </div>
     </div>
