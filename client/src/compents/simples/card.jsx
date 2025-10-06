424c43037964e544fbc5eba503dce9126d11354b
IZ4NA <jovijukes.cvg@gmail.com>
2025-10-06 11:10:14 -0300

    up dialogCard

--- client/src/compents/simples/card.jsx
+++ client/src/compents/simples/card.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import "./card.css"
+import "./card.css";
 
 export default function Card() {
-    return (
-        <div className='card'>
-            <div className='conteiner-card'>
+  const [isOpen, setIsOpen] = useState(false);
 
-                <p>teste de card</p>
-            </div>
+  return (
+    <div className="card">
+      <div className="conteiner-card">
+        <div>
+          <h3>teste de dialog</h3>
+          <p>aqui eu estou testando um dialog simples com um redenrizaçãocondicional</p>
         </div>
-    );
+        <button onClick={() => setIsOpen(true)} className="buttom-dialog">Abrir</button>
+
+        {isOpen && (
+          <div className="dialogOverlay">
+            <div className="dialogOpem">
+              <div>
+                <h3>Dialog Aberto</h3>
+                <p>Deseja realmente fechar?</p>
+              </div>
+              <button onClick={() => setIsOpen(false)}>Cancelar</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
- 
