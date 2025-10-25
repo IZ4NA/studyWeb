d8830a59fea6e1a59b8adfae450117e4b183b77f
IZ4NA <jovijukes.cvg@gmail.com>
2025-10-25 12:08:10 -0300

    moch up

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css'; 
 
+import "./mocks/server.js"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {/* renderiza o App */}
+    <App /> 
   </React.StrictMode>
 );
--- client/src/mocks/server.js
+++ client/src/mocks/server.js
@@ -1,1 +1,53 @@
-import { ce} from "miragejs"
+import { createServer } from "miragejs";
+
+
+createServer({
+    routes() {
+        this.namespace = "api";
+
+        let list = [
+            { id: "1", label: "teste 1", complete: false },
+            { id: "2", label: "teste 2", complete: false },
+            { id: "3", label: "teste 3", complete: false },
+            { id: "4", label: "teste 4", complete: false },
+        ]
+
+        this.get("/todo", (schema) => {
+            return schema.all(list);
+        });
+
+        this.post("/item", (schema, request) => {
+            const newItemBody = JSON.parse(request.requestBody)
+
+            const createItem = schema.create(list, newItemBody)
+            
+            return createItem ; 
+
+            // newItem.id = String(list.length + 1)
+            // list.push(newItem)
+            // return newItem;
+
+        })
+
+
+        this.put("/atualizar/:id", (schema, request) => {
+            const id = request.params.id
+            const updatedBody = JSON.parse(request.requestBody)
+
+            const atuaList = schema.find(list, id)
+            atuaList.update(updatedBody)
+
+            return;
+        })
+
+        this.delete("/delete/:id", (schema, request)=>{
+            const id = request.params.id
+
+            const desTreoy = schema.find(list, id)
+
+            desTreoy.destroy();
+
+            return;
+        })
+    },
+});
